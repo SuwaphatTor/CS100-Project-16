1e4367f8ca8d2da365f12734cd9561d5526a7a90
TeamMie4K <Teamthai101@gmail.com>
2023-12-09 01:47:55 +0700

    test2

--- CSTU Passport/JS/script.js
+++ CSTU Passport/JS/script.js
@@ -171,7 +171,7 @@ async function submitForm(event) {
     end_date: formData.get("endDate"),
     location: formData.get("location"),
     description: formData.get("description"),
-    picture: formData.get(reader.result)
+    picture: formData.get("recent-image")
   };
 
   console.log(data);
