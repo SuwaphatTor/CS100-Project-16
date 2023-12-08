621bdc95f4617627cfde63c67a01713bba0cb7aa
TeamMie4K <Teamthai101@gmail.com>
2023-12-08 20:28:29 +0700

    delete all

--- CSTU Passport/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("button1").addEventListener("click", function() {
-    window.location.href = './HTML5';
-});
-
-document.getElementById("button2").addEventListener("click", function() {
-    window.location.href = './JS';
-});
