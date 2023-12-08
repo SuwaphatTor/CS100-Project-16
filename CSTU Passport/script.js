88822c6f6fc94b352fcbff2df8c660cb522aa10e
TeamMie4K <Teamthai101@gmail.com>
2023-12-08 20:04:06 +0700

    try ta website in our repo

---
+++ CSTU Passport/script.js
@@ -0,0 +1,7 @@
+document.getElementById("button1").addEventListener("click", function() {
+    window.location.href = './HTML5';
+});
+
+document.getElementById("button2").addEventListener("click", function() {
+    window.location.href = './JS';
+});
